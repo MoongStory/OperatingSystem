a8a646de7dcb843507e07774767a20dc8d20870a
Moong <moongstory@gmail.com>
2022-06-06 13:35:13 +0900

    trim 함수 제거

--- OperatingSystem/OperatingSystem.h
+++ OperatingSystem/OperatingSystem.h
@@ -23,22 +23,6 @@ namespace MOONG
 		static const std::string GetWindowsVersion();
 	protected:
 	private:
-		//앞에 있는 개행 문자 제거
-		static inline std::string& ltrim(std::string& s) {
-			s.erase(s.begin(), std::find_if(s.begin(), s.end(), std::not1(std::ptr_fun<int, int>(std::isspace))));
-			return s;
-		}
-
-		//뒤에 있는 개행 문자 제거
-		static inline std::string& rtrim(std::string& s) {
-			s.erase(std::find_if(s.rbegin(), s.rend(), std::not1(std::ptr_fun<int, int>(std::isspace))).base(), s.end());
-			return s;
-		}
-
-		//양쪽 끝의 개행 문자 제거
-		static inline std::string& trim(std::string& s) {
-			return ltrim(rtrim(s));
-		}
 	};
 }
 #endif	// _OPERATINGSYSTEM_H_
