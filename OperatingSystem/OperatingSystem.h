aa2b4cc3ad695393a942ab9e05cbb075f7d36420
Moong <moongstory@gmail.com>
2022-06-08 23:29:01 +0900

    추후 Windows 버전 체크 관련 작업이 필요할 경우 참고할 수 있는 링크 주석으로 추가

--- OperatingSystem/OperatingSystem.h
+++ OperatingSystem/OperatingSystem.h
@@ -21,6 +21,16 @@ namespace MOONG
 
 	public:
 		static const std::string GetWindowsProductName();
+		
+		// Windows 버전 관련은 아래 링크 참고.
+		// 
+		// Windows 체크하는 함수들.
+		// 참고 - https://docs.microsoft.com/en-us/windows/win32/sysinfo/version-helper-apis
+		// 
+		// 현재 실행중인 Windows가 지정된 요구 사항을 충족하는지 체크.
+		// 참고 - https://docs.microsoft.com/en-us/windows/win32/api/Winbase/nf-winbase-verifyversioninfoa
+		// 
+		// 참고 - https://docs.microsoft.com/en-us/windows/win32/api/winnt/ns-winnt-osversioninfoexa
 	protected:
 	private:
 	};
